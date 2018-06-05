95b41cc6a10489cada85904192e0525c8cfc231b
Jonas Tranberg <jonas_ts@msn.com>
2018-06-05 15:39:32 +0200

    start

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
-import astor, ast
+import astor, ast, sys
 from termcolor import colored
+
+# Global prefs
+
+pif_secret_label = ast.Str('_pif_secret')
+pif_public_label = ast.Str('_pif_public')
 
 # Main functions
 
 def load_ast(file):
     return astor.code_to_ast.parse_file(file)
 
-def walk(node):
+def walk(node, fn):
     if hasattr(node, 'body'):
-        node.body = [walk(n) for n in node.body]
+        node.body = [walk(n, fn) for n in node.body]
         return node
     else:
-        return handleType(node)
+        return fn(node)
 
-def handleType(node):
+# Used for analysis run
+def handleAnalyse(node):
     t = get_node_type(node)
     ln, col = get_node_pos(node)
 
     if t == 'Assign':
-        return handle_assign(node, ln, col)
+        if get_node_type(node.value.elts[0]) == 'Name':
+            name_conf = get_name_confidential_label(node.value.elts[0])
+            tuple_conf = get_tuple_confidentiality_label(node.value)
+            
+            if tuple_conf != name_conf:
+                printb(source[ln-1][col:].replace('\n', ''), ln, col)
 
-    if t == 'Name':
-        return node
-
-    if t == 'Value':
-        return node
-    
-    if t == 'Num':
         return node
 
     # no handler defined for node, just return it
+    return node
+
+# Used for labeling run
+def handleLabel(node):
+    t = get_node_type(node)
+    ln, col = get_node_pos(node)
+
+    if t == 'Assign':
+        l = pif_public_label
+        if get_name_confidential(node.targets[0]):
+            l = pif_secret_label
+
+        t = ast.Tuple([
+            node.value,
+            l
+        ], None)
+
+        node.value = t
+
     return node
 
 # Helping functions
@@ -40,32 +63,53 @@ def get_node_type(node):
 def get_node_pos(node):
     return (node.lineno, node.col_offset)
 
+def get_tuple_confidentiality_label(node):
+    if is_pif_tuple(node):
+        return node.elts[1]
+    else:
+        raise Exception('Can not get conf rating on none tuple!')
+
 def get_name_confidential(node):
     if get_node_type(node) == 'Name':
         return node.id.endswith('_secret') 
     else:
-        raise Exception('Can not get confidential of none Name node!')
+        raise Exception('Can not get confidential of {} node!'.format(get_node_type(node)))
+
+def get_name_confidential_label(node):
+    if get_name_confidential(node):
+        return pif_secret_label
+    else:
+        return pif_public_label
+
+def is_pif_tuple(node):
+    return get_node_type(node) == 'Tuple' and (node.elts[1] == pif_public_label or node.elts[1] == pif_secret_label)
+
+# Prints
 
 def printw(msg, ln, col):
-    print(colored('({}, {}) WARNING:'.format(ln, col), 'yellow'), msg)
+    print(colored('WARNING ({}, {}):'.format(ln, col), 'yellow'), msg)
+
+def printb(msg, ln, col):
+    print(colored('Confidentiality breach ({}, {}):'.format(ln, col), 'red'), msg)
+    exit(-1)
 
 # Handling functions
 
-def handle_assign(node, ln, col):
-    l = ast.Str('public')
-    if get_name_confidential(node.targets[0]):
-        l = ast.Str('secret')
-
-    t = ast.Tuple([
-        node.value,
-        l
-    ], None)
-
-    node.value = t
-    return node
+...
 
 # Main
-main_ast = load_ast('./tests/assign1.py')
-new_ast = walk(main_ast)
-source = astor.to_source(new_ast)
-print(source)
+main_ast = load_ast(sys.argv[1])
+source = open(sys.argv[1]).readlines()
+
+new_ast_label = walk(main_ast, handleLabel)
+new_ast_analysed = walk(new_ast_label, handleAnalyse)
+
+new_source = astor.to_source(new_ast_analysed)
+
+print('SOURCE ---------------------------------')
+print(''.join(source))
+print('RESULT ---------------------------------')
+print(new_source)
+print('EXEC -----------------------------------')
+
+exec(new_source)
