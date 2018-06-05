10760e3499a654eaab249bd2ed3bef06673c4b69
Jonas Tranberg <jonas_ts@msn.com>
2018-06-05 13:17:48 +0200

    reset

--- pifcore/parser.py
+++
@@ -1,8 +0,0 @@
-import parso, os
-
-class Parser:
-    def parse(self, path):
-        p = os.path.join(os.path.dirname(os.path.realpath(__file__)),"Grammar/grammar37_pif")
-        grammar = parso.load_grammar(path=p)
-        with open(path) as f:
-            return grammar.parse(f.read())
