3b8282a3fbd3afd9e6d5ef107108872a3a28a26e
Jonas Tranberg <jonas_ts@msn.com>
2018-06-06 12:13:59 +0200

    new structure

--- main.py
+++ main.py
@@ -11,15 +11,17 @@ pif_public_label = ast.Str('_pif_public')
 def load_ast(file):
     return astor.code_to_ast.parse_file(file)
 
-def walk(node, fn):
+#
+# LABELING
+# 
+def doLabeling(node):
     if hasattr(node, 'body'):
-        node.body = [walk(n, fn) for n in node.body]
+        node.body = [doLabeling(n) for n in node.body]
         return node
     else:
-        return fn(node)
+        return labelNode(node)
 
-# Used for labeling run
-def handleLabel(node):
+def labelNode(node):
     t = get_node_type(node)
     ln, col = get_node_pos(node)
 
@@ -37,31 +39,110 @@ def handleLabel(node):
 
     return node
 
-# Used for analysis run
-def handleAnalyse(node):
+#
+# ANALYSIS
+# 
+def doAnalysis(node, pc=None, label=None):
+    # default values
+    if not pc:
+        pc = [pif_public_label]
+
+    if not label:
+        label = pif_public_label
+
+    # Walk!
+    if hasattr(node, 'body'):
+        [doAnalysis(n, pc, label) for n in node.body]
+    else:
+        analyseNode(node, pc, label)
+
+def analyseNode(node, pc, label):
     t = get_node_type(node)
     ln, col = get_node_pos(node)    
 
-    if t == 'Assign':
-        return checkAssign(node, ln, col)
+    # stmt
+    if t == 'Expr':
+        return (node, pc, label)
+    elif t == 'Assign':
+        return (node, pc, label)
+    elif t == 'Compare':
+        return (node, pc, label)
+    elif t == 'If':
+        return (node, pc, label)
+    elif t == 'While':
+        return (node, pc, label)
+    elif t == 'For':
+        return (node, pc, label)
+    elif t == 'Pass':
+        return (node, pc, label)
+    elif t == 'Break':
+        return (node, pc, label)
+    elif t == 'Continue':
+        return (node, pc, label)
+
+    # expr
+    elif t == 'Name':
+        return (node, pc, label)
+    elif t == 'Num':
+        return (node, pc, label)
+    elif t == 'Str':
+        return (node, pc, label)
+    elif t == 'BoolOp':
+        return (node, pc, label)
+    elif t == 'BinOp':
+        return (node, pc, label)
+    elif t == 'Compare':
+        return (node, pc, label)
+    elif t == 'UnaryOp':
+        return (node, pc, label)
+    elif t == 'IfExp':
+        return (node, pc, label)
+    elif t == 'Tuple':
+        return (node, pc, label)
+    elif t == 'List':
+        return (node, pc, label)
+    elif t == 'Dict':
+        return (node, pc, label)
+    elif t == 'Set':
+        return (node, pc, label)
+    elif t == 'Subscript':
+        return (node, pc, label)
+
+    # boolop
+    elif t == 'And':
+        return (node, pc, label)
+    elif t == 'Or':
+        return (node, pc, label)
+
+    # operator
+    elif t == 'Add':
+        return (node, pc, label)
+    elif t == 'Sub':
+        return (node, pc, label)
+    elif t == 'Mult':
+        return (node, pc, label)
+    elif t == 'Div':
+        return (node, pc, label)
+    elif t == 'Mod':
+        return (node, pc, label)
+    elif t == 'Pow':
+        return (node, pc, label)
+
+    # unaryop
+    elif t in ['Invert', 'Not', 'UAdd', 'USub']:
+        return (node, pc, label)
+
+    # cmpop
+    elif t in ['Eq', 'NotEq', 'Lt', 'LtE', 'Gt', 'Gte', 'Is', 'IsNot']:
+        return (node, pc, label)
+    elif t in ['In', 'NotIn']:
+        return (node, pc, label)
 
     # no handler defined for node, just return it
-    return node
+    printw('Unsupported code {} 😭'.format(get_source_at(ln, col)), ln, col)
+    return (node, pc, label)
 
 # Handling functions
-
-def checkAssign(node, ln, col):
-    t = get_node_type(node.value.elts[0])
-
-    if t == 'Name':
-        name_conf = get_name_confidential_label(node.value.elts[0])
-        tuple_conf = get_tuple_confidentiality_label(node.value)
-        
-        if tuple_conf != name_conf:
-            printb(source[ln-1][col:].replace('\n', ''), ln, col)
-
-    return node
-
 
 # Helping functions
 
@@ -92,6 +173,9 @@ def get_name_confidential_label(node):
 def is_pif_tuple(node):
     return get_node_type(node) == 'Tuple' and (node.elts[1] == pif_public_label or node.elts[1] == pif_secret_label)
 
+def get_source_at(ln, col):
+    return source[ln-1][col:].replace('\n', '')
+
 # Prints
 
 def printw(msg, ln, col):
@@ -105,10 +189,9 @@ def printb(msg, ln, col):
 main_ast = load_ast(sys.argv[1])
 source = open(sys.argv[1]).readlines()
 
-new_ast_label = walk(main_ast, handleLabel)
-new_ast_analysed = walk(new_ast_label, handleAnalyse)
-
-new_source = astor.to_source(new_ast_analysed)
+new_ast_label = doLabeling(main_ast)
+doAnalysis(new_ast_label)
+new_source = astor.to_source(new_ast_label)
 
 print('SOURCE ---------------------------------')
 print(''.join(source))
--- tests/assign2_fail.py
+++ tests/assign2_fail.py
@@ -2,4 +2,6 @@ a_secret = 2
 b_public = 42
 
 a_secret = b_public
+b_public = a_secret
 
+[x+1 for x in [1, 2, 3]]
